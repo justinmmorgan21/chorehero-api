2da34fd3f15848eb0a12843e595efa49a4d29d6d
justinmmorgan21 <justinmorgan21@gmail.com>
2025-01-16 18:09:51 -0600

    completed UsedReward create, index, and update actions

--- app/controllers/used_rewards_controller.rb
+++ app/controllers/used_rewards_controller.rb
@@ -1,13 +1,32 @@
 class UsedRewardsController < ApplicationController
   def create
-    
+    @used_reward = UsedReward.new(
+      child_id: params[:child_id],
+      reward_id: params[:reward_id],
+      date_approved: nil
+    )
+    if @used_reward.save
+      render :show, status: :created
+    else
+      render json: { errors: @used_reward.errors.full_messages }, status: :bad_request
+    end
   end
 
   def index
-    
+    @used_rewards = UsedReward.all
+    render :index
   end
 
   def update
-    
+    @used_reward = UsedReward.find_by(id: params[:id])
+    if @used_reward
+      @used_reward.assign_attributes(params.permit(:child_id, :reward_id, :date_approved).compact_blank)
+    end
+    if @used_reward.save!
+      render :show
+    else
+      render json: { error: @used_reward.errors.full_messages },
+      status: :unprocessable_entity
+    end
   end
 end
